ca69324fb7fae117e8c19c87def927182039ee73
Luis Miguel Cutiopala <61071133+cutioluis@users.noreply.github.com>
2021-10-03 16:39:57 -0500

    FIX new libraries

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../Layout/Layout";
+
+const Home = () => {
+  return (
+    <div>
+      <Layout></Layout>
+    </div>
+  );
+};
+
+export default Home;
